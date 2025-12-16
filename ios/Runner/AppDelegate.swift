f1666b8d401095e5699f6254a9d575b00cd5ad81
khoitn1 <khoitn1@hdbank.com.vn>
2025-12-16 10:06:36 +0700

    al1

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,18 @@
-import Flutter
 import UIKit
+import Flutter
+import UserNotifications
 
-@main
+@UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
+
+    // 🔴 BẮT BUỘC: gán delegate cho notification
+    UNUserNotificationCenter.current().delegate = self
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
